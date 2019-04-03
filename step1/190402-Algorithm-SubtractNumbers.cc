1f52bcb4846e340906a2b9344b59d6843c4c93b7
sonypark <sonypark0204@gmail.com>
2019-04-04 00:47:15 +0900

    Change directory

---
+++ step1/190402-Algorithm-SubtractNumbers.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    using std::cout;
+    using std::cin;
+    
+    int a, b, sub;
+    
+    cin >> a >> b;
+    
+    sub = a - b;
+    
+    cout << sub;
+    
+    return 0;
+}
