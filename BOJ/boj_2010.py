c10498239a362f63018d7529390f140ad85e15b6
sonypark <sonypark0204@gmail.com>
2019-05-15 16:50:03 +0900

    [BOJ] 2010

---
+++ BOJ/boj_2010.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+s = 0
+for i in range(n):
+    # a = int(sys.stdin.readline())
+    # s +=a
+    print(sum(map(int, sys.stdin.readline())))
+# print (s - (n-1))
+
+
+## 다른 사람 풀이
