eb6814cb4111bd4a7fd20e02a0723f08ea2d53a6
sonypark <sonypark0204@gmail.com>
2019-05-20 20:48:57 +0900

    [BOJ] 2445

---
+++ BOJ/boj_2445.py
@@ -0,0 +1,7 @@
+import sys
+n = int(sys.stdin.readline())
+for i in range(1,n+1):
+    print('*'*(i) + ' ' * 2*(n-i) + '*' * (i))
+for i in range(n-1,0,-1):
+    print('*'*(i) + ' ' * 2*(n-i) + '*' * (i))
+
