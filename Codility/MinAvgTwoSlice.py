5a5a6f601c9edf39ce85f870f9aa65a6f0d4b472
Sony <sonypark0204@gmail.com>
2019-09-02 19:56:33 +0900

    [Codility] MinAvgTwoSlice

---
+++ Codility/MinAvgTwoSlice.py
@@ -0,0 +1,9 @@
+# def minAvgTwoSlice(A):
+#     count = [[]] * len(A)
+#     print(count)
+#
+#     for i,v in enumerate(A):
+#         count[i].append(A[])
+#     print(count)
+# a = [4,2,2,5,1,5,8]
+# minAvgTwoSlice(a)
